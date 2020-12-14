8d55c8aaa3893547409b05c4685e2b5c7e96202a
Miguel Ferrer <ingferrermiguel@gmail.com>
2020-12-13 20:08:05 -0500

    Add get_deal_detail and query_deals method

--- clientify/deals.py
+++ clientify/deals.py
@@ -22,6 +22,14 @@ class Deals(object):
         endpoint = '/deals/'
         return self.client._get(self.client.BASE_URL + endpoint)
 
+    def get_deal_details(self, deal_id):
+        endpoint = '/deals/{}/'.format(deal_id)
+        return self.client._get(self.client.BASE_URL + endpoint)
+
+    def query_deals(self, params: dict):
+        endpoint = '/deals/'
+        return self.client._get(self.client.BASE_URL + endpoint, params=params)
+
     def create_deal(self, data: dict):
         endpoint = '/deals/'
         return self.client._post(self.client.BASE_URL + endpoint, json=data)
